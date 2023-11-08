8b731a79bce83ce60b345a9dcc6cdbf9adf05886
Sean Lester Durham <sean.durham1982@gmail.com>
2023-11-07 19:22:50 -0600

    added contrast

--- pythonImage_editor/photoEditor.py
+++ pythonImage_editor/photoEditor.py
@@ -13,6 +13,9 @@ for filename in os.listdir(path):
     
     edit = img.filter(ImageFilter.SHARPEN)
     
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(factor)
+    
     clean_name = os.path.splitext(filename)[0]
     
     edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
